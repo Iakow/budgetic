7b11a3d41df7a2775160ba7a8e42ca67fe56f4c1
iakow <ponomarykov@gmail.com>
2019-11-17 22:20:18 +0100

    added firebase & porbe adding docs into firestore

--- src/components/AddPurchase.js
+++ src/components/AddPurchase.js
@@ -1,16 +1,63 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 import * as ROUTES from '../constants/routes';
 
-const AddPurchase = () => (
-  <div>
-    <h1>Добавить покупку</h1>
-    
-    <Link to={ROUTES.MAIN}>
-      На главную
-    </Link>
-  </div>
-)
+class AddPurchase extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '', submit: false};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    if(this.state.value) {
+      this.props.db.collection("users").add({
+        first: this.state.value
+      })
+      .then(function(docRef) {
+          console.log("Document written with ID: ", docRef.id);
+          
+      })
+      .catch(function(error) {
+          console.error("Error adding document: ", error);
+      });
+      this.setState({submit: true})
+    } else {
+      alert('Поле пустое')
+    }
+
+    event.preventDefault();
+  }
+
+  render() {
+    if (this.state.submit) {
+      return <Redirect to="/" />
+    } else {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Сумма:
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+
+            <input type="submit" value="Отправить" />
+          </form>
+      
+          <Link to={ROUTES.MAIN}>
+            На главную
+          </Link>
+        </div>
+      );    
+    }
+  }
+}
 
 export default AddPurchase;
--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,69 @@
 import React from 'react';
 import {Switch, Route} from "react-router-dom";
+import * as firebase from 'firebase/app';
+import "firebase/firestore";
 
 import * as ROUTES from '../constants/routes';
 
 import List from './List';
 import AddPurchase from './AddPurchase';
-import Main from './Main'
+import Main from './Main';
 
-const App = () => (
-  <div>
-    <Switch>
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.db = null;
+  }
+  componentWillMount() {
+
+    var firebaseConfig = {
+      apiKey: "AIzaSyDGOIDSMbZruufHctcPZqGdzVXnJwCJAg0",
+      authDomain: "budgetic-fcb0e.firebaseapp.com",
+      databaseURL: "https://budgetic-fcb0e.firebaseio.com",
+      projectId: "budgetic-fcb0e",
+      storageBucket: "budgetic-fcb0e.appspot.com",
+      messagingSenderId: "808634940866",
+      appId: "1:808634940866:web:623d627c6700ea69a6aa5b"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+
+    this.db = firebase.firestore();
+
+/*     db.collection("users").add({
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    }); */
+  }
+
+
+  render() {
+    return (
+      <div>
+      <Switch>
         <Route path={ROUTES.ADD}>
-            <AddPurchase />
+          <AddPurchase db = {this.db}/>
         </Route>
 
         <Route path={ROUTES.STATS}>
-            <List />
+          <List />
         </Route>
 
         <Route path={ROUTES.MAIN}>
-            <Main />
+          <Main />
         </Route>
-    </Switch>
-  </div>
-);
+      </Switch>
+    </div>
+    )
+  }
+}
 
 export default App;
