121b72c5078b9f503c59add2e9dbc1432bd719c9
Yakov <ponomarykov@gmail.com>
2020-08-30 20:53:16 +0200

    Tabs.jsx

---
+++ src/components/Stats/Tabs.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Tabs extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tab: 1
+    }
+  }
+
+  render () {
+    return;
+  }
+}
+
+export default Tabs;
