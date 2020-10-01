990d32a37c257f4e39b63f54ea3cf7c3a5ca9891
Yakov <ponomarykov@gmail.com>
2020-10-01 22:32:40 +0200

    Вынес всю лишнюю логику из Stats ниже.

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Table from './Table';
 import Tabs from './Tabs'
 import DateFilter from './DateFilter';
-import SortingBlock from './SortingBlock'
-
-/* !!! ПО ХОРОШЕМУ СОРТИРОВКА ДОЛЖНА ПРОИСХОДИТЬ В САМОМ СПИСКЕ */
+import ActionsList from './ActionsList';
 
 class Stats extends React.Component {
   constructor(props) {
@@ -14,12 +11,7 @@ class Stats extends React.Component {
 
     this.state = {
       firstDate: transactions[transactions.length - 1].date,
-      lastDate: transactions[0].date,
-
-      sortingBySum: 'off',
-      sortingByDate: 'off',
-      sortingByCategory: 'off',
-      sortingByTags: 'off'
+      lastDate: transactions[0].date
     }
   }
 
@@ -38,89 +30,8 @@ class Stats extends React.Component {
   }
 
 
-  handleSorting = (e) => {
-    const name = e.target.name;
-
-    const getNextSwitchValue = (currentValue) => {
-      const opts = ['off', 'up', 'down'];
-
-      const currentIndex = opts.findIndex((item) => item === currentValue);
-
-      return opts[currentIndex + 1] || opts[0];
-    };
-
-    if (name === 'sortingBySum') this.setState({ sortingByDate: 'off' });
-    if (name === 'sortingByDate') this.setState({ sortingBySum: 'off' });
-    if (name === 'sortingByCategory') this.setState({ sortingByTags: 'off' });
-    if (name === 'sortingByTags') this.setState({ sortingByCategory: 'off' });
-
-    this.setState((state) => ({ [name]: getNextSwitchValue(state[name]) }));
-  }
-
-
-  getSortedList = () => {
-    let list = this.getDateFilteredTransactions();
-
-    const sortBy = (list, prop, switcher) => {
-      if (this.state[switcher] === 'off') {
-        return list;
-      }
-  
-      if (this.state[switcher] === 'up') {
-        return list.sort((a, b) => {
-          if (a[prop] > b[prop]) return 1;
-          if (a[prop] < b[prop]) return -1;
-          return 0;
-        })
-      }
-  
-      if (this.state[switcher] === 'down') {
-        return list.sort((a, b) => {
-          if (a[prop] < b[prop]) return 1;
-          if (a[prop] > b[prop]) return -1;
-          return 0;
-        })
-      }
-    }
-
-    list = sortBy(list, 'sum', 'sortingBySum');
-    list = sortBy(list, 'date', 'sortingByDate');
-    list = sortBy(list, 'category', 'sortingByCategory');
-
-    return list;
-  }
-
-
   render() {
-    const {
-      firstDate,
-      lastDate,
-      sortingBySum,
-      sortingByDate,
-      sortingByCategory,
-      sortingByTags
-    } = this.state;
-
-    const list = this.getSortedList();
-
-    const tab1 = (
-      <>
-        <SortingBlock
-          bySum={sortingBySum}
-          byDate={sortingByDate}
-          byCategory={sortingByCategory}
-          byTags={sortingByTags}
-          handler={this.handleSorting}
-        />
-
-        <Table
-          db={this.props.db}
-          transactions={list}
-          tags={this.props.tags}
-          categories={this.props.categories}
-        />
-      </>
-    )
+    const { firstDate, lastDate } = this.state;
 
     return (
       <>
@@ -131,7 +42,7 @@ class Stats extends React.Component {
         />
 
         <Tabs
-          tabs={[tab1, "Diagramm"]}
+          tabs={[<ActionsList />, "Diagramm"]}
           titles={["Table", "Diagramm"]}
         />
       </>
